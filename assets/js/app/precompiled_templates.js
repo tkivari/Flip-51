a5f55ef42b39f98fd4fe5aac3f159576e364933c
Tyler Kivari <ty.kivari@gmail.com>
2016-02-28 14:00:16 -0500

    add merchant logos

--- assets/js/app/precompiled_templates.js
+++ assets/js/app/precompiled_templates.js
@@ -3,14 +3,16 @@
 templates['deal_item'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "            <div class=\"sale_item\">\n                <div id=\"price\" class=\"title\">"
+  return "            <div class=\"sale_item\">\n                <div class=\"merchant_logo\"><img src=\""
+    + alias4(((helper = (helper = helpers.merchant_logo || (depth0 != null ? depth0.merchant_logo : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"merchant_logo","hash":{},"data":data}) : helper)))
+    + "\"/></div>\n                <div class=\"deal_info\">\n                    <div id=\"price\" class=\"title\">"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.current_price : depth0),{"name":"if","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + alias4(((helper = (helper = helpers.merchant || (depth0 != null ? depth0.merchant : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"merchant","hash":{},"data":data}) : helper)))
-    + "</div>\n                <div id=\"name\">"
+    + "</div>\n                    <div id=\"name\">"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</div>\n                <div id=\"extra_info\">"
+    + "</div>\n                    <div id=\"extra_info\">"
     + alias4(((helper = (helper = helpers.sale_story || (depth0 != null ? depth0.sale_story : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"sale_story","hash":{},"data":data}) : helper)))
-    + "</div>\n\n            </div>\n";
+    + "</div>\n                </div>\n                <div class=\"clear\"></div>\n            </div>\n";
 },"2":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
